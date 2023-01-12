08b3f051a647dcc354c825f8488c18ffa56a7845
Olle Lukowski <ollel@pop-os.localdomain>
2023-01-12 11:23:07 +0100

    another test

--- cookie_generator.js
+++ cookie_generator.js
@@ -1,9 +1,21 @@
 console.log("Hello World!");
-var autoplay;
+let autoplay;
 if (!autoplay) autoplay = {};
-
+/*
 Game.ClickCookie();
 for (let index = 0; index < 0; index++) {
   console.log("E");
   Game.ClickCookie();
+}
+*/
+
+autoplay.run = function () {
+  for (let sx in Game.shimmers) {
+    console.log("found shimmer");
+    let s = Game.shimmers[sx];
+    if (s.type == "golden") {
+      s.pop();
+      console.log("popped shimmer");
+    }
+  }
 }
